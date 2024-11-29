bf12b86402e6e2fec127b096f08b1966595cff12
Sapondanai Sriwan <sapondanaisriwan@gmail.com>
2024-11-29 15:22:20 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,31 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int even, odd = 0;
+    int number;
+    while (true)
+    {
+
+        cout << "Enter an integer: ";
+        cin >> number;
+
+        if (number == 0)
+        {
+            cout << "#Even numbers = " << even << endl;
+            cout << "#Odd numbers = " << odd;
+            break;
+        }
+
+        if (number % 2 == 0)
+        {
+            even = even + 1;
+        }
+        else
+        {
+            odd += 1;
+        }
+    }
     return 0;
 }
